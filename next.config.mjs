76aeef454df11ae362904c145c0da53a3f205899
Edwin Hernandez <Edwinhern.16@gmail.com>
2024-02-12 01:38:13 -0600

    Refactor files structure (#4)
    * refactor: add favicon.ico, global types, and layout component***
    
    ***Update next.config.mjs and tsconfig.json***
    
    ***Add global CSS styles
    
    * chore: add @next/bundle-analyzer package and configure Next.js bundle analysis workflow
    
    * refactor: add layout and page components
    
    * refactor: update action-download-artifact version to v3
    
    * refactor: add bundle artifact download step

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
