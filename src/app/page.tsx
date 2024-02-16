b9c2115763379cc2805301040910963b630cace1
Edwin Hernandez <edwinhern.16@gmail.com>
2024-02-16 11:21:04 -0600

    feat: add icons and links to deploy and star on Vercel and Github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-import { IconCoffee } from '@tabler/icons-react';
+import { IconBrandGithub, IconBrandVercel, IconCoffee } from '@tabler/icons-react';
 import { Mountain } from 'lucide-react';
 import Link from 'next/link';
 
 import { BentoGridThirdDemo } from '@/components/Landing';
 import { buttonVariants } from '@/components/ui/button';
 import { ThemeToggleButton } from '@/components/ui/theme-toggle-button';
+import { cn } from '@/lib/utils';
 
 export default function Home() {
   return (
     <main className="flex size-full min-h-lvh flex-col justify-between gap-8 p-5 md:p-10">
       {/* Navbar */}
       <div className="flex size-full items-center justify-between">
-        <Mountain />
+        <div className="flex items-center justify-center gap-2">
+          <Mountain />
+          <h1 className="font-heading text-lg font-bold">NextHorizon</h1>
+        </div>
         <ThemeToggleButton />
       </div>
 
@@ -25,7 +29,29 @@ export default function Home() {
             An opinionated Next.js Boilerplate that is not just about starting; it&apos;s about starting right.
             Here&apos;s what sets it apart:
           </span>
+          <div className="mx-auto mt-6 flex items-center justify-center space-x-5">
+            {/* Deploy to Vercel & Start on Github */}
+            <Link
+              rel="noopener noreferrer"
+              target="_blank"
+              className={cn('', buttonVariants({ variant: 'default' }))}
+              href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fedwinhern%2Fnext-template-2024&project-name=next-template-2024&repository-name=next-template-2024&demo-title=Next.js%202024%20Template&demo-description=An%20advanced%20Next.js%20template%20equipped%20with%20TailwindCSS%2C%20TypeScript%2C%20and%20essential%20developer%20tools.&demo-url=https%3A%2F%2Fyour-demo-url.com"
+            >
+              <IconBrandVercel className="mr-2 size-4" /> Deploy to Vercel
+            </Link>
+            {/*                href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+             */}
+            <Link
+              rel="noopener noreferrer"
+              target="_blank"
+              className={buttonVariants({ variant: 'outline' })}
+              href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fedwinhern%2Fnext-template-2024&project-name=next-template-2024&repository-name=next-template-2024&demo-title=Next.js%202024%20Template&demo-description=An%20advanced%20Next.js%20template%20equipped%20with%20TailwindCSS%2C%20TypeScript%2C%20and%20essential%20developer%20tools.&demo-url=https%3A%2F%2Fyour-demo-url.com"
+            >
+              <IconBrandGithub className="mr-2 size-4" /> Star on Github
+            </Link>
+          </div>
         </div>
+
         <BentoGridThirdDemo />
       </div>
 
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import { Analytics } from '@vercel/analytics/react';
+import { PropsWithChildren } from 'react';
 
 import { ThemeProvider } from '@/providers/theme-provider';
 
-export const Providers: React.FC<ChildProps> = ({ children }) => {
+export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <ThemeProvider>
       {children}
