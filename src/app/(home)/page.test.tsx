f0479223c8f96499990c37d91d6cd2046e706d1c
Edwin Hernandez <edwinhern.16@gmail.com>
2024-08-05 01:27:21 -0500

    chore: :heavy_minus_sign: Remove test files

--- src/app/(home)/page.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-
-import HomePage from "@/app/(home)/page";
-
-describe("Home Component", () => {
-  it("renders the component successfully", () => {
-    render(<HomePage />);
-  });
-});
